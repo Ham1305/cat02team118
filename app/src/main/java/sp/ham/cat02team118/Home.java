683c639f30a27ad43110b9509487640de44b692f
Ham1305 <mkwiisg@gmail.com>
2021-06-14 21:10:46 +0800

    BrainHack Code exp draft

--- app/src/main/java/sp/ham/cat02team118/Home.java
+++ app/src/main/java/sp/ham/cat02team118/Home.java
@@ -6,15 +6,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
+    private Animation bottomhalf;
+    //private LinearLayout LL1;
+    private ScrollView homescroller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomhalf = AnimationUtils.loadAnimation(this,R.anim.bottom_half);
+        //LL1 = findViewById(R.id.linearLayout1);
+        homescroller = findViewById(R.id.homescroller);
+        //LL1.setAnimation(bottomfast);
+        homescroller.setAnimation(bottomhalf);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.rewardsNavViewBar);
 
