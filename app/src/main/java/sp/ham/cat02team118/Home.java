103cc33b003d06e77a8e95ac430f87227b007072
TSL08 <tsllay8@gmail.com>
2021-06-14 18:49:32 +0800

    added code for bottom navigation bar for home, explore, activity and settings

---
+++ app/src/main/java/sp/ham/cat02team118/Home.java
@@ -0,0 +1,49 @@
+package sp.ham.cat02team118;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.rewardsNavViewBar);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        return true;
+
+                    case R.id.explore:
+                        startActivity(new Intent(getApplicationContext(), Explore.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.activity:
+                        startActivity(new Intent(getApplicationContext(), History.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.nav_settings:
+                        startActivity(new Intent(getApplicationContext(), Settings.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
