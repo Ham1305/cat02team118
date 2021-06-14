ea8c88aa191486f764dd83f059783b632c6ed29b
Ham1305 <mkwiisg@gmail.com>
2021-06-15 03:36:42 +0800

    Added Splashscreen, login and registration layouts, logic WIP

---
+++ app/src/main/java/sp/ham/cat02team118/SplashScreen.java
@@ -0,0 +1,29 @@
+package sp.ham.cat02team118;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intenthome = new Intent(SplashScreen.this, Login.class);
+                startActivity(intenthome);
+                finish();
+            }
+        },2000);
+    }
+}
