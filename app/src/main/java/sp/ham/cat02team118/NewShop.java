ee39a82b23affa512c91a21f0d55a537465902f1
Ham1305 <mkwiisg@gmail.com>
2021-06-16 03:10:15 +0800

    DONE LOGIN AND REGISTER WITH FIRESTORE USER INFO (excpet forgot password)

---
+++ app/src/main/java/sp/ham/cat02team118/NewShop.java
@@ -0,0 +1,14 @@
+package sp.ham.cat02team118;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewShop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_shop);
+    }
+}
