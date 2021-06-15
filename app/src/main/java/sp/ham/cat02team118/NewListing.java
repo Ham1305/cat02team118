e165775ac2f24de079461a8176d2e692e141a614
Ham1305 <mkwiisg@gmail.com>
2021-06-16 03:27:43 +0800

    added layouts for creating new store and listings (logic wip)

---
+++ app/src/main/java/sp/ham/cat02team118/NewListing.java
@@ -0,0 +1,14 @@
+package sp.ham.cat02team118;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_listing);
+    }
+}
