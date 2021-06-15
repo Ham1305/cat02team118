5763b7dc3a8cf3aebd02a270e2d385d26dd91ffe
Ham1305 <mkwiisg@gmail.com>
2021-06-15 12:16:50 +0800

    DONE LOGIN AND REGISTER WITH FIRESTORE USER INFO (excpet forgot password)

--- app/src/main/java/sp/ham/cat02team118/Login.java
+++ app/src/main/java/sp/ham/cat02team118/Login.java
@@ -6,10 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
-    Button RegBut;
+    Button RegBut,LogBut;
+    EditText username,password;
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +26,41 @@ public class Login extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
         RegBut = findViewById(R.id.Regbutton);
+        LogBut = findViewById(R.id.Logbutton);
+
+        auth = FirebaseAuth.getInstance();
+
         RegBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this,Register.class);
                 startActivity(intent);
+            }
+        });
+
+        LogBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = username.getText().toString();
+                String passStr = password.getText().toString();
+                loginUser(emailStr,passStr);
+            }
+        });
+
+
+    }
+
+    private void loginUser(String email, String password){
+        auth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(Login.this, "Login Successful!" , Toast.LENGTH_SHORT).show();;
+                startActivity(new Intent(Login.this , Home.class));
+                finish();
             }
         });
     }
--- app/src/main/java/sp/ham/cat02team118/Register.java
+++ app/src/main/java/sp/ham/cat02team118/Register.java
@@ -21,6 +21,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.Autocomplete;
@@ -32,21 +33,27 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    private EditText firstname,lastname,username,password,address,email;
+    public static final String TAG = "TAG";
+    private EditText firstname,lastname,username,password,address,email,phone;
     private Button regconfirm;
 
     private FirebaseAuth auth;
-    private FirebaseStorage storage;
+    private FirebaseFirestore firestore;
 
     private double uLat=0,uLon=0;
     private String country = "SG";
+    private String UserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +63,16 @@ public class Register extends AppCompatActivity {
 
         firstname = findViewById(R.id.firstname);
         lastname = findViewById(R.id.lastname);
-        username = findViewById(R.id.username);
+        username = findViewById(R.id.usernamereg);
         email = findViewById(R.id.emailreg);
         password = findViewById(R.id.passreg);
         address = findViewById(R.id.enter_address);
         regconfirm = findViewById(R.id.regconfirm);
+        phone = findViewById(R.id.phonereg);
         EditText confirm = findViewById(R.id.confirmpass);
 
         auth = FirebaseAuth.getInstance();
-        storage = FirebaseStorage.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         Places.initialize(getApplicationContext(),"AIzaSyATnQKwhcuyoLSZpDTIMnESETadwd0pyYU");
 
@@ -94,11 +102,13 @@ public class Register extends AppCompatActivity {
                 String confirmStr = confirm.getText().toString();
                 String firstnameStr = firstname.getText().toString();
                 String lastnameStr = lastname.getText().toString();
-                //String addressStr
+                String addressStr = address.getText().toString();
+                String usernameStr = username.getText().toString();
+                String phoneStr = phone.getText().toString();
 
-                /*String uLatStr = String.valueOf(uLat);
+                String uLatStr = String.valueOf(uLat);
                 String uLonStr = String.valueOf(uLon);
-                Toast.makeText(getApplicationContext(), uLatStr + "  ,  " + uLonStr,Toast.LENGTH_SHORT).show();*/
+                //Toast.makeText(getApplicationContext(), uLatStr + "  ,  " + uLonStr,Toast.LENGTH_SHORT).show();
 
                 if(TextUtils.isEmpty(emailStr) || TextUtils.isEmpty(passStr)){
                     Toast.makeText(Register.this," Please enter blank fields! ", Toast.LENGTH_LONG).show();
@@ -107,7 +117,7 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this," Passwords do not match! ", Toast.LENGTH_LONG).show();
                 }
                 else{
-                    registerUser(emailStr , passStr);//wip
+                    registerUser(emailStr , passStr,firstnameStr,lastnameStr,addressStr,usernameStr,phoneStr,uLatStr,uLonStr);//wip
 
                 }
             }
@@ -147,12 +157,30 @@ public class Register extends AppCompatActivity {
         }
     }
 
-    private void registerUser(String email, String password){
+    private void registerUser(String email, String password, String firstname, String lastname, String address, String username, String phoneno, String mLatStr, String mLonStr){
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    UserID = auth.getCurrentUser().getUid();
+                    DocumentReference documentReference = firestore.collection("users").document(UserID);
+                    Map<String,Object> user = new HashMap<>();
+                    user.put("email",email);
+                    user.put("firstname",firstname);
+                    user.put("lastname",lastname);
+                    user.put("username",username);
+                    user.put("phoneno",phoneno);
+                    user.put("address",address);
+                    user.put("uLat",mLatStr);
+                    user.put("uLon",mLonStr);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.d(TAG, "onSuccess: user Profile is created for "+ UserID);
+                        }
+                    });
                     Toast.makeText(Register.this, "Register successful!", Toast.LENGTH_SHORT).show();
+                    finish();
                 } else  {
                     Toast.makeText(Register.this, "something went wrong...", Toast.LENGTH_SHORT).show();
                 }
