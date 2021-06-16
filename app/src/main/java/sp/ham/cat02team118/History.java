a974f72a5d739bf4fbff89e791193097674f007d
Ham1305 <mkwiisg@gmail.com>
2021-06-16 22:15:50 +0800

    SETUP SHOP & ITEM PAGES DONE, WAITING FOR SHOP PROFILE, PLACEHOLDED FOR NOW

--- app/src/main/java/sp/ham/cat02team118/History.java
+++ app/src/main/java/sp/ham/cat02team118/History.java
@@ -41,8 +41,8 @@ public class History extends AppCompatActivity {
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.nav_settings:
-                        startActivity(new Intent(getApplicationContext(), NewShop.class));
+                    case R.id.business:
+                        startActivity(new Intent(getApplicationContext(), ProfilepageOwners.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
