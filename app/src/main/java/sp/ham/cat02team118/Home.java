3a2911893ce6cf727cf9dcd4c66ae007dcc1128c
weiw3n <chuaww2002@gmail.com>
2021-06-16 15:35:44 +0800

    owner page-half way

--- app/src/main/java/sp/ham/cat02team118/Home.java
+++ app/src/main/java/sp/ham/cat02team118/Home.java
@@ -76,8 +76,8 @@ public class Home extends AppCompatActivity {
                         overridePendingTransition(0,0);
                         return true;
 
-                    case R.id.nav_settings:
-                        startActivity(new Intent(getApplicationContext(), Settings.class));
+                    case R.id.business:
+                        startActivity(new Intent(getApplicationContext(), ProfilepageOwners.class));
                         overridePendingTransition(0,0);
                         return true;
                 }
